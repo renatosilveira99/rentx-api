57c68655b900f4c9291c527363cbcdbcb2253817
Renato Silveira de Lima <renatosilveira99@hotmail.com>
2022-10-26 14:27:52 -0300

    Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,9 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import '@shared/container';
 
 import { AppError } from '@shared/errors/AppError';
@@ -21,6 +24,18 @@ const app = express();
 
 app.use(rateLimiter)
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -30,6 +45,8 @@ app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
